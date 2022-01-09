889b93ffbde7bc758e0f1a6777e4e27145ec5b40
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2022-01-09 11:00:44 +0100

    add boilerplate

---
+++ rom.asm
@@ -0,0 +1,30 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+;  Atari 2600 boilerplate
+;  Written by Stanley Skarshaug 09.01.2022
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    processor 6502
+
+    seg code
+    org $F000       ; Define the code origin at $F000
+
+Start:
+    sei             ; Disable iterrupts
+    cld             ; Disable BCD decimal math math mode
+    ldx #$FF        ; Load the X register with #$FF. NB. literally load $FF into the register!
+    txs             ; Transfer the X register to the Stack pointer register
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Code goes here!
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Fill the ROM size to exactly 4KB
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    org $FFFC
+    .word Start     ; Reset vector at $FFFC (where the program starts)
+    .word Start     ; Iterrup vector at $FFFE (unused int the VCS, but required by 6502 assembly)
